a7238dfb7df0063453a6c467ca2428e1e95e33d5
kato atsushi <ka.baseball1997@gmail.com>
2021-04-14 12:38:27 +0900

    get media

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React from 'react';
 
-function App() {
+
+// async function getMedia() {
+//   const constraints = { audio: true, video: true };
+  // let stream = null;
+
+  // try {
+  //   stream = await navigator.mediaDevices.getUserMedia(constraints);
+  //   /* ストリームを使用 */
+  // } catch(err) {
+  //   /* エラーを処理 */
+  // }
+// }
+const getMedia  = async () => {
+  const constraints = { audio: true, video: true };
+  try {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+    /* ストリームを使用 */
+  } catch(err) {
+    /* エラーを処理 */
+  }
+}
+
+getMedia();
+
+const App = () =>  {
   return <div>Hello, React!</div>;
 }
 
